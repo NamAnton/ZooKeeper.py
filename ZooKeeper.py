61892719818b6be7178355a720a28489e5868b03
NamAnton <91003780+NamAnton@users.noreply.github.com>
2023-01-12 13:29:41 +0200

    Update ZooKeeper.py

--- ZooKeeper.py
+++ ZooKeeper.py
@@ -120,8 +120,6 @@ Switching on the camera in the rabbit habitat...
 It looks like we will soon have more rabbits!"""
 
 animals = [camel, lion, deer, goose, bat, rabbit]
-
-# write your code here
 while True:
     try:
         choose = int(input("Please enter the number of the habitat you would like to view:"))
